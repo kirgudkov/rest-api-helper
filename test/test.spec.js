02e80bcfb196155c0c3747fba656018ef7753bfc
Kirill Gudkov <for.example.nickname@gmail.com>
2018-06-30 01:48:09 +0300

    Покрыл часть кода тестами

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,70 @@
 import { RestApiHelper } from "../src/controller";
 import 'chai/register-should';
-const assert = require('assert');
+import 'chai/register-assert';
+import 'chai/register-expect';
 
 describe('_getBody', function() {
-	it('should return null when method === get or GET or head or HEAD', function() {
+
+	it('should return null when method === get', function() {
 		assert.equal(RestApiHelper._getBody('get', {}), null);
+	});
+
+	it('should return null when method === GET', function() {
 		assert.equal(RestApiHelper._getBody('GET', {}), null);
+	});
+
+	it('should return null when method === head', function() {
 		assert.equal(RestApiHelper._getBody('head', {}), null);
+	});
+
+	it('should return null when method === HEAD', function() {
 		assert.equal(RestApiHelper._getBody('HEAD', {}), null);
 	});
+
+	it('should return second arg in JSON when method !== get or GET or head or HEAD', function() {
+		assert.equal(RestApiHelper._getBody('post', {}), '{}');
+	});
+
+});
+
+describe('_getMethod', function() {
+
+	it('should return method from config if it match RFC 2616 specification', function() {
+		assert.equal(RestApiHelper._getMethod('get'), 'GET');
+	});
+
+	it(`should return Error if argument doesn't match RFC 2616 specification`, () => {
+		(function() {
+			RestApiHelper._getMethod('test')
+		}).should.Throw(Error, 'Invalid method')
+	});
+
+});
+
+describe('_decorate', function() {
+	it('should return json if status means success', function() {
+		RestApiHelper.configure({ "successStatus": [200] });
+		assert(RestApiHelper._decorate({ status: 200, json: {} }), {})
+	});
+
+	it(`should return Error status doesn't means success`, () => {
+		(function() {
+			RestApiHelper.configure({ "successStatus": [] });
+			RestApiHelper._decorate({ status: 200, json: {} })
+		}).should.Throw(Error)
+	});
+});
+
+describe('_isSuccess', function() {
+
+	it('should return true if status belong _config.successStatus array', function() {
+		RestApiHelper.configure({ "successStatus": [200] });
+		assert.equal(RestApiHelper._isSuccess(200), true);
+	});
+
+	it('should return false if status belong _config.successStatus array', function() {
+		RestApiHelper.configure({ "successStatus": [] });
+		assert.equal(RestApiHelper._isSuccess(200), false);
+	});
+
 });
