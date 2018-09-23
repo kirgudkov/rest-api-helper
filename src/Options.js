e0ae5455a6f02346b8bcb9960ebf5ec90066de32
Kirill Gudkov <for.example.nickname@gmail.com>
2018-09-23 20:52:46 +0300

    v0.0.13rc append features: - append form-url-encoded content-type supporting

--- src/Options.js
+++ src/Options.js
@@ -1,6 +1,6 @@
 import FormData from 'form-data';
-import { getQueryParameters } from './utils';
-import config from '../config/config';
+import { getQueryParameters, getFormURLEncodedBody } from './utils';
+import RFC from '../config/config';
 
 export class Options {
 	constructor(request, baseURL, headers) {
@@ -9,72 +9,103 @@ export class Options {
 		this._globalHeaders = headers;
 	}
 
-  setRequest(request) {
-    if (request.method === '') {
-      throw new Error(`Method can't be empty string`);
-    }
-    else if (typeof request.headers !== 'object') {
-      throw new Error(`Headers should be an Object`);
-    }
-    else if (Array.isArray(request.headers)) {
-      throw new Error(`Headers should be an Object`);
-    }
-    else {
-      this.request = request;
-    }
-  }
-
-  setBaseURL(url) {
-    this.baseURL = url;
-  }
-
-  getOptions() {
-    return {
-      method: this.getMethod(),
-      url: this.getUrl(this.request.url),
-      headers: this.getHeaders(this.request.headers),
-      body: this.getBody(this.request.body),
-    };
-  }
-
-  getUrl() {
-    const queryParams = this._isBodyNotAllowed(this.getMethod()) ? getQueryParameters(this.request.body) : '';
-    return this.getRequestUrl() + queryParams;
-  }
-  getRequestUrl() {
-    if (this.request.url.indexOf('https://') !== -1 || this.request.url.indexOf('http://') !== -1) {
-      return this.request.url;
-    }
-    let baseURL = this.baseURL || '';
-    return baseURL + this.request.url;
-  }
-
-	getHeaders() {
-		return {...this._globalHeaders, ...this.request.headers} || {};
+	setRequest(request) {
+		if (request.method === '') {
+			throw new Error(`Method can't be empty string`);
+		}
+		else if (typeof request.headers !== 'object') {
+			throw new Error(`Headers should be an Object`);
+		}
+		else if (Array.isArray(request.headers)) {
+			throw new Error(`Headers should be an Object`);
+		}
+		else {
+			this.request = request;
+		}
 	}
 
-  getBody() {
-    if (this.request.body instanceof FormData) {
-      return this.request.body;
-    }
-    if (this._isBodyNotAllowed(this.request.method)) {
-      return null;
-    }
-    return JSON.stringify(this.request.body);
-  }
+	setBaseURL(url) {
+		this.baseURL = url;
+	}
 
-  getMethod() {
-    // Only methods from the RFC 2616 specification are allowed
-    if (config.method[this.request.method]) {
-      return config.method[this.request.method];
-    }
-    else {
-      throw new Error(`Invalid method ${this.request.method}`);
-    }
-  }
+	getOptions() {
+		return {
+			method: this.getMethod(),
+			url: this.getUrl(this.request.url),
+			headers: this.getHeaders(this.request.headers),
+			body: this.getBody(this.request.body),
+		};
+	}
 
-  _isBodyNotAllowed(method) {
-    const lowerCaseMethod = method.toLowerCase();
-    return lowerCaseMethod === 'get' || lowerCaseMethod === 'head';
-  }
+	getUrl() {
+		const queryParams = this._isBodyNotAllowed(this.getMethod()) ? getQueryParameters(this.request.body) : '';
+		return this.getRequestUrl() + queryParams;
+	}
+
+	getRequestUrl() {
+		if (this.request.url.indexOf('https://') !== -1 || this.request.url.indexOf('http://') !== -1) {
+			return this.request.url;
+		}
+		let baseURL = this.baseURL || '';
+		return baseURL + this.request.url;
+	}
+
+	getHeaders() {
+		return this._objectToLowerCase({...this._globalHeaders, ...this.request.headers} || {});
+	}
+
+	getBody() {
+		if (this._isBodyNotAllowed(this.request.method)) {
+			return null;
+		}
+
+		if (this._isFormURLEncoded()) {
+			return getFormURLEncodedBody(this.request.body);
+		}
+
+		if (this.request.body instanceof FormData) {
+			return this.request.body;
+		}
+
+		return JSON.stringify(this.request.body);
+	}
+
+	getMethod() {
+		// Only methods from the RFC 2616 specification are allowed
+		if (RFC.method[this.request.method]) {
+			return RFC.method[this.request.method];
+		}
+		else {
+			throw new Error(`Invalid method ${this.request.method}`);
+		}
+	}
+
+	_isBodyNotAllowed(method) {
+		const lowerCaseMethod = method.toLowerCase();
+		return lowerCaseMethod === 'get' || lowerCaseMethod === 'head';
+	}
+
+	_isFormURLEncoded() {
+		let isFormUrlEncoded = false;
+		Object.keys(this.request.headers).map(header => {
+			if (this.request.headers[header].toLowerCase() === 'application/x-www-form-urlencoded') {
+				isFormUrlEncoded = true;
+			}
+		});
+
+		return isFormUrlEncoded;
+	}
+
+	_objectToLowerCase(obj) {
+		let result = {};
+
+		Object.keys(obj).map(key => {
+			let lowerKey = key.toString().toLowerCase();
+			const lowerProp = obj[key].toString().toLowerCase();
+
+			return result[lowerKey] = lowerProp;
+		});
+
+		return result;
+	}
 }
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,37 @@
-
 export function copyObject(obj) {
-  return JSON.parse(JSON.stringify(obj));
+	return JSON.parse(JSON.stringify(obj));
 }
 
 function getValue(key, value) {
-  return `${key}=${value}`;
+	return `${key}=${value}`;
 }
 
 export function getQueryParameters(body) {
-  if (!body) {
-    return '';
-  }
-  let res = '';
-  const parameters = Object.keys(body);
-  for (let i = 0; i < parameters.length; ++i) {
-    const parameterName = parameters[i];
-    if (body[parameterName] !== undefined) {
-      res += `${res.length === 0 ? '?' : '&'}${getValue(parameterName, body[parameterName])}`;
-    }
-  }
-  return res;
+	if (!body) {
+		return '';
+	}
+	let res = '';
+	const parameters = Object.keys(body);
+	for (let i = 0; i < parameters.length; ++i) {
+		const parameterName = parameters[i];
+		if (body[parameterName] !== undefined) {
+			res += `${res.length === 0 ? '?' : '&'}${getValue(parameterName, body[parameterName])}`;
+		}
+	}
+	return res;
+}
+
+export function getFormURLEncodedBody(body) {
+
+	let formBody = [];
+
+	for (let property in body) {
+		if (body.hasOwnProperty(property)) {
+			let encodedKey = encodeURIComponent(property);
+			let encodedValue = encodeURIComponent(body[property]);
+			formBody.push(encodedKey + "=" + encodedValue);
+		}
+	}
+
+	return formBody.join("&");
 }
