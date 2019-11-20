8a0216c251472401ebd452b437d5593332beee2a
kirillgudkov <for.example.nickname@gmail.com>
2019-11-20 15:38:11 +0300

    updated index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -31,27 +31,27 @@ interface Headers {
   [key: string]: string;
 }
 
-export interface Response {
+export interface Response<T> {
   status: number;
-  body: any;
+  body: T;
   headers: Headers;
 }
 
-export interface Request {
-  withHeaders(headers: Headers): Request;
-  withBody(body: Body | string): Request;
-  shouldBeIntercepted(value: boolean): Request;
-  withQueryParams(params: QueryParams): Request;
-  withUrlParam(name: string, value: string | number): Request;
+export interface Request<T> {
+  withHeaders(headers: Headers): Request<T>;
+  withBody(body: Body | string): Request<T>;
+  shouldBeIntercepted(value: boolean): Request<T>;
+  withQueryParams(params: QueryParams): Request<T>;
+  withUrlParam(name: string, value: string | number): Request<T>;
   /*
    * @deprecated - use withUrlParam
    */
-  withParam(name: string, value: string | number): Request;
-  fetch(): Promise<Response>;
+  withParam(name: string, value: string | number): Request<T>;
+  fetch(): Promise<Response<T>>;
 }
 
-export interface Interceptor {
-  delegate: (response: Response) => void;
+export interface Interceptor<T> {
+  delegate: (response: Response<T>) => void;
 }
 
 export class RestApiHelper {
@@ -59,11 +59,11 @@ export class RestApiHelper {
    * @deprecated - use withConfig
    */
   static configure(config: Config): void;
-  static build(method: string): Request;
+  static build<T>(method: string): Request<T>;
 
   static builder(): RestApiHelper;
   withConfig(config: any): RestApiHelper;
-  withInterceptor(interceptor: Interceptor): RestApiHelper;
+  withInterceptor<T>(interceptor: Interceptor<any>): RestApiHelper;
 }
 
 export default RestApiHelper;
